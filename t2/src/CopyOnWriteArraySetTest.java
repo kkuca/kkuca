c9db519e56ee07f486a097f3f7918f7a8c626dad
Test Spojení <kkuca@logis.cz>
2024-07-24 11:19:34 +0200

    4

--- t2/src/CopyOnWriteArraySetTest.java
+++ t2/src/CopyOnWriteArraySetTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class CopyOnWriteArraySetTest {
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+public class CopyOnWriteArraySetTest {
+    public static void main(String args[])  {
+        Set<String> copyOnWriteArraySet = new CopyOnWriteArraySet<>();
+        copyOnWriteArraySet.add("a");
+        copyOnWriteArraySet.add ("b");
+        Iterator<String> iterator = copyOnWriteArraySet.iterator();
+        while(iterator.hasNext()) {
+            copyOnWriteArraySet.add("c");
+            System.out.println(iterator.next());
+        }
+        System.out.println("----------");
+        iterator = copyOnWriteArraySet.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
